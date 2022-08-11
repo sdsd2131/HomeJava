009c30e6748c705801f7f7760b3a18c57700dbae
sdsd2131 <p31982522@gmail.com>
2022-08-11 22:27:57 +0900

    List Map Test Commit

--- src/test/testCode.java
+++ src/test/testCode.java
@@ -1,5 +1,10 @@
 package test;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class testCode {
 	public static void main(String[] args) {
 		System.out.println("Github Test...!!");
@@ -7,5 +12,20 @@ public class testCode {
 		System.out.println("Github Test2..!!");
 		
 		System.out.println("HiHello??");
+		
+		List<Map<String, String>> list = new ArrayList<Map<String,String>>();
+		Map<String, String> map = new HashMap<String, String>();
+		map.put("name", "Yeri");
+		map.put("age", "28");
+		list.add(map);
+		
+		map.put("name", "Jongeun");
+		map.put("age", "29");
+		list.add(map);
+		
+		for(int i=0; i<list.size(); i++) {
+			System.out.println("list.get(" + i + ") = " + list.get(i).get("name"));
+			System.out.println("list.get(" + i + ") = " + list.get(i).get("age"));
+		}
 	}
 }
